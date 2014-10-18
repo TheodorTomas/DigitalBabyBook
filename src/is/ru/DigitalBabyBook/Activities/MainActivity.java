1a628e1227494755791a3ddb32e10ccecc5199fb
sindris12 <sindris12@ru.is>
2014-10-18 16:54:04 +0000

    new buttons

--- src/is/ru/DigitalBabyBook/Activities/MainActivity.java
+++ src/is/ru/DigitalBabyBook/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package is.ru.DigitalBabyBook.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
